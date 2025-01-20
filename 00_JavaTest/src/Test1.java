381187db3cb8f728dd30a526387eb4dbf5fbd437
ggkama <ggkama@naver.com>
2025-01-20 11:25:24 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,30 @@
+// JS와 주석 동일
+
+/* Run 버튼 클릭시 (단축기 ctrl + f11)
+ * 1) 자동으로 컴파일 수행(javac ooo.jave) -> .class 파일 생성
+ *    -> .class 파일(byte code) 생성
+ *    
+ * 2) 컴파일 완료 후 .class 파일 실행 (java 파일명)
+ */
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("이클립스 실행 중1");
+		System.out.println("이클립스 실행 중2");
+		System.out.println("이클립스 실행 중3");
+		System.out.println("이클립스 실행 중4");
+	
+		/* Java API DOC: 
+		 * Java에서 제공해주는 코드의 사용법이 설명 되어있는 문서
+		 * 
+		 * DOC 확인 방법
+		 * 1) google -> java 21 api 검색
+		 * 
+		 * 2) 궁금한 코드 클릭 -> shift + f12
+		 */
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,13 @@
+public class JavaTest{
+
+   public static void main(String[] args){
+   
+      System.out.println("Hello World");
+      System.out.println("안녕하세요");
+      System.out.println("Hello World");
+     
+      for(int i=1 ; i<=50  ; i++){
+	System.out.println(i + "번째 출력");
+	}
+   }
+}
